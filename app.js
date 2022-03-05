87532f853fa378a5b72260343dffee55be6dc790
HACKERqq420 <fhjalsa@gmail.com>
2022-03-05 22:46:58 +0300

    a

--- app.js
+++ app.js
@@ -3,3 +3,14 @@
 function changeTime() {
   document.querySelector(".clock").innerHTML = Date();
 }
+
+function any() {
+    const list = document.getElementsByTagName("hr");
+    for (let i = 0; i < list.length; i++) {
+        list[i].before("hello");
+;    }
+}
+
+setInterval(changeTime, 1000);
+
+any();
