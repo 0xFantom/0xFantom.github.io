e517fe28ab2e0da917fe60e989a741e4ad4b0e03
HACKERqq420 <fhjalsa@gmail.com>
2022-03-06 23:38:20 +0300

    very cool

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 // make a function that changes the text of the class clock into the current time
 
-function changeTime() {
-  let time = new Date();
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  let seconds = time.getSeconds();
-  let clock = document.querySelector(".clock");
-  // make hours into 12-hour format
-  if (hours > 12) {
-    hours = hours - 12;
-  }
-  // add a zero in front of numbers less than 10
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  // put the time together
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
-}
+let slideSpeed = 500;
 
-setInterval(changeTime, 1000);
+// clock with jQuery
+$(document).ready(function() {
+  $("#portable").click(() => {
+    $("#portable .repo-desc").toggle(slideSpeed);
+    $(`#portable .doc`).slideToggle(slideSpeed);
+  });
+  $("#ll").click(() => {
+    $("#ll .repo-desc").toggle(slideSpeed);
+    $(`#ll .doc`).slideToggle(slideSpeed);
+  });
+
+  setInterval(function() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    // 24 hour to 12 hour
+    if (hours > 12) {
+      hours = hours - 12;
+    }
+    let formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    $('.clock').text(formattedTime);
+  }, 1000);
+});
